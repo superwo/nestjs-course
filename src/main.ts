3ba8a8bc4dab7fe5ed81060dd2efdc685868f508
Radu <radu.avadanii@gmail.com>
2025-04-23 18:00:38 +0300

    upd: api description.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * Use validation pipes globally
+   */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +15,22 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  /**
+   * swagger configuration
+   */
+  const config = new DocumentBuilder()
+    .setVersion('1.0')
+    .setTitle('NestJS API - Blog app API')
+    .setDescription('NestJS API description')
+    .setTermsOfService('http://localhost:3300/terms-of-service')
+    .setLicense('MIT', 'http://localhost:3300/license')
+    .addServer('http://localhost:3300', 'Development server')
+    .build();
+  // Instaniate Document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3300);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,12 +13,35 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
 import { UsersService } from './providers/users.service';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('{/:id}')
+  @ApiOperation({
+    summary: 'Fetches a list of registered users on the application',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Users fetched successfully based on the query parameters',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'number',
+    required: false,
+    description: 'The number of entries returned per query',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: false,
+    description: 'The position of the page to be returned',
+    example: 1,
+  })
   public getUsers(
     @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
