ca685f45eb468e03b3b635133627016162f3bd98
Radu <radu.avadanii@gmail.com>
2025-04-24 15:50:38 +0300

    upd: create post.

---
+++ src/posts/dtos/create-post.dto.ts
@@ -0,0 +1,67 @@
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { postStatus } from '../enums/postStatus.enum';
+import { postType } from '../enums/postType.enum';
+import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
+import { Type } from 'class-transformer';
+
+export class CreatePostDto {
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
+  title: string;
+
+  @IsEnum(postType)
+  @IsNotEmpty()
+  postType: postType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message: 'A slug should be all small letters, numbers, and hyphens',
+  })
+  slug: string;
+
+  @IsEnum(postStatus)
+  @IsNotEmpty()
+  status: postStatus;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsOptional()
+  @IsJSON()
+  schema?: string;
+
+  @IsOptional()
+  @IsUrl()
+  featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
+  publishOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
+  tags?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreatePostMetaOptionsDto)
+  metaOptions: CreatePostMetaOptionsDto[];
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dtos/create-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -18,5 +19,10 @@ export class PostsController {
   @Get('{/:userId}')
   public getPosts(@Param('userId') userId: string) {
     return this.postsService.findAll(userId);
+  }
+
+  @Post()
+  public createPost(@Body() createPostDto: CreatePostDto) {
+    console.log(createPostDto);
   }
 }
