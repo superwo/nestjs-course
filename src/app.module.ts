1ccd6a074d4e6dee8f2810cfb459bb65979f0e50
Radu <radu.avadanii@gmail.com>
2025-04-25 15:41:09 +0300

    upd: user entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [User],
+        synchronize: true,
+        port: 5432,
+        username: 'postgres',
+        password: 'artemida',
+        host: 'localhost',
+        database: 'nestjs-blog',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
