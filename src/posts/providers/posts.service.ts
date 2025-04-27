1867eead57d34ded4b91450ae71b064e7bbea97f
Radu <radu.avadanii@gmail.com>
2025-04-27 12:55:04 +0300

    upd: eager post meta options loading.

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,10 +1,9 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { CreatePostDto } from '../dtos/create-post.dto';
 import { Repository } from 'typeorm';
 import { Post } from '../post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MetaOption } from 'src/meta-options/meta-option.entity';
 
 @Injectable()
 export class PostsService {
@@ -18,69 +17,27 @@ export class PostsService {
      */
     @InjectRepository(Post)
     private readonly postsRepository: Repository<Post>,
-    /**
-     * Inject metaOptionsRepository to access the database
-     */
-    @InjectRepository(MetaOption)
-    private readonly metaOptionsRepository: Repository<MetaOption>,
   ) {}
 
   /**
    * Creating new post
    */
   public async create(createPostDto: CreatePostDto) {
-    // Create the metaOptions first if they exist
-    const metaOptions = createPostDto.metaOptions
-      ? this.metaOptionsRepository.create(createPostDto.metaOptions)
-      : null;
-
-    if (metaOptions) {
-      await this.metaOptionsRepository.save(metaOptions);
-    }
-
     // Create the post
     const post = this.postsRepository.create({
       ...createPostDto,
-      metaOptions: createPostDto.metaOptions || undefined,
+      metaOptions: createPostDto.metaOptions ?? undefined,
     });
-
-    // If meta options exist add them to post
-    if (metaOptions) {
-      post.metaOptions = metaOptions;
-    }
 
     return await this.postsRepository.save(post);
   }
 
-  public findAll(userId: string) {
-    const user = this.usersService.findOneById(userId); // Using the UsersService to find a user by ID
+  public async findAll(userId: string) {
+    const user = this.usersService.findOneById(userId);
+    console.log(user);
 
-    return [
-      {
-        user: user,
-        title: 'Post 1',
-        content: 'Content of Post 1',
-      },
-      {
-        user: user,
-        title: 'Post 2',
-        content: 'Content of Post 2',
-      },
-      {
-        user: user,
-        title: 'Post 3',
-        content: 'Content of Post 3',
-      },
-      {
-        user: user,
-        title: 'Post 4',
-        content: 'Content of Post 4',
-      },
-      {
-        user: user,
-        title: 'Post 5',
-        content: 'Content of Post 5',
-      },
-    ];
+    const posts = await this.postsRepository.find({});
+
+    return posts;
   }
 }
